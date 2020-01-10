e388f93c5e55dccfb2a48e89814294a7e646a3e9
Eric D <eric.daddio@gmail.com>
2020-01-10 20:11:18 +0000

    introduce map links.

--- src/modules/init.js
+++ src/modules/init.js
@@ -1,25 +1,35 @@
-
 let path = window.location.hash.split('/');
+let is_home = false;
 
-if (path[1] === '') {
-  console.log('home');
+if (path.length < 2 || path[1] === '') {
+  is_home = true;
+  path = ["#!home", "sphere-of-influence"];
 }
 
 function reload() {
+  throw new Error((function(){
     window.location.reload(true);
+  })());
 }
 
 window.addEventListener('popstate', reload);
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
 
-async function getMapJson() 
-{
+async function getMapIndex() {
+  let response = await fetch(`/maps/index.json`);
+  let data = await response.json();
+  return data;
+}
+
+async function getMapJson() {
   let response = await fetch(`/maps/${path[1]}.json`);
   let data = await response.json();
   return data;
 }
 
-async function requestTweets(demand, mapJson) 
-{
+async function requestTweets(demand, mapJson) {
   let response = await fetch(`http://0.0.0.0:5000/?demand=${demand}`, {
     method: 'POST',
     headers: {
@@ -32,66 +42,89 @@ async function requestTweets(demand, mapJson)
   return data;
 }
 
-function setupMapPage(options) {
-    
-    const page = {
-        nameEls: document.querySelectorAll('.js-map-name'),
-        strapEls: document.querySelectorAll('.js-map-strap'),
-        followingEl: document.querySelector('.js-map-following'),
-        followingLeadEl: document.querySelector('.js-map-following-lead'),
-        rootEl: document.documentElement
-    };
-    
-    page.nameEls.forEach(element => {
-        element.innerHTML = options.name;
+function setupPage(options) {
+
+  const page = {
+    nameEls: document.querySelectorAll('.js-map-name'),
+    strapEls: document.querySelectorAll('.js-map-strap'),
+    followingEl: document.querySelector('.js-map-following'),
+    followingLeadEl: document.querySelector('.js-map-following-lead'),
+    metaTitleEls: document.querySelectorAll('[property="og:title"], [property="twitter:title"]'),
+    metaDescriptionEls: document.querySelectorAll('[property="og:description"], [property="twitter:description"]'),
+    mapListEl: document.querySelector('.js-map-list'),
+    rootEl: document.documentElement
+  };
+
+
+  page.nameEls.forEach(element => {
+    element.innerHTML = options.name;
+  });
+
+  page.strapEls.forEach(element => {
+    element.innerHTML = options.strap;
+  });
+
+  page.metaTitleEls.forEach(element => {
+    element.setAttribute('content', options.name);
+  });
+
+  page.metaDescriptionEls.forEach(element => {
+    element.setAttribute('content', options.strap);
+  });
+
+  options.handles = options.handles || [];
+  if (options.handles.length > 0) {
+    options.handles.forEach(handle => {
+      const link = document.createElement('a');
+      link.href = `//twitter.com/${handle}`;
+      link.innerHTML = `@${handle}`;
+      page.followingEl.appendChild(link);
+      link.insertAdjacentHTML('afterend', ' ');
     });
+  } else {
+    page.followingLeadEl.parentElement.removeChild(page.followingLeadEl);
+  }
 
-    page.strapEls.forEach(element => {
-        element.innerHTML = options.strap;
+  page.rootEl.style.setProperty('--color', options.color);
+
+  document.title = `${options.name} - ${options.strap}`;
+
+  getMapIndex().then(index => {
+    Object.keys(index).forEach(function(slug) {
+      const link = document.createElement('a');
+      link.href = `/#!/${slug}`;
+      link.classList.add('more-map-link');
+      link.innerHTML = index[slug];
+      page.mapListEl.appendChild(link);
+      link.insertAdjacentHTML('beforebegin', '<br>');
     });
-
-    options.handles = options.handles || [];
-    if (options.handles.length > 0) {
-      options.handles.forEach(handle => {
-          const link = document.createElement('a');
-          link.href = `//twitter.com/${handle}`;
-          link.innerHTML = `@${handle}`;
-          page.followingEl.appendChild(link);
-          link.insertAdjacentHTML('afterend', ' ');
-      });
-    } else {
-          page.followingLeadEl.parentElement.removeChild(page.followingLeadEl);
-    }
-
-    page.rootEl.style.setProperty('--color', options.color);
-
-    document.title = `${options.name} - ${options.strap}`
+  });
 
 }
 
 getMapJson()
   .then(mapJson => {
 
-    setupMapPage(mapJson);
+    setupPage(mapJson);
     window.initMap(mapJson);
 
     requestTweets('stale', mapJson)
-    .then(data => {
-      window.captureTweets(data.tweets);
-    }); 
-  
+      .then(data => {
+        window.captureTweets(data.tweets);
+      });
+
     requestTweets('fresh', mapJson)
-    .then(data => {
-      window.captureTweets(data.tweets);
-    }); 
-      
+      .then(data => {
+        window.captureTweets(data.tweets);
+      });
+
   })
   .catch(reason => {
     console.error(reason);
-      const options = {
-        name : "404 - Map not found",
-        strap : "We couldn't find a map here, is the link correct?",
-        color : ""
-      };
-      setupMapPage(options);
+    const options = {
+      name: "404 - Map not found",
+      strap: "We couldn't find a map here, is the link correct?",
+      color: ""
+    };
+    setupPage(options);
   });
