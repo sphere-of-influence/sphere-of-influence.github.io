51a353eef816b9d0a840fb40b2a15d2bec82a75b
Eric D <eric.daddio@gmail.com>
2020-08-04 19:00:08 +0100

    remove marxist slogan

--- src/app.js
+++ src/app.js
@@ -2,6 +2,3 @@ import './modules/scroll';
 import './modules/twitter';
 import './modules/map';
 import './modules/init';
-
-// eslint-disable-next-line no-console
-console.log('Workers of the world, unite!');
